0b9f20a8c6ad272916e4b14b34df40e2643b1328
SmartHomeBeginner <htpcbeginner@gmail.com>
2019-03-27 12:59:08 -0400

    Update mysql_configure.sh

--- init/mysql_configure.sh
+++ init/mysql_configure.sh
@@ -3,26 +3,29 @@
 # mysql_configure.sh
 #
 
-if [[ condition1 && condition2 ]]; then
+ZM_DB_HOST=${ZM_DB_HOST:-localhost}
+ZM_DB_PORT=${ZM_DB_PORT:-3306}
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-mysqlpsswd}
+ZM_DB_USER=${ZM_DB_USERNAME:-zmuser}
+ZM_DB_PASS=${ZM_DB_PASSWORD:-zmpass}
+	
+if [[ $ZM_DB_HOST == "localhost" && $MYSQL_ROOT_PASSWORD != "mysqlpsswd" ]]; then
+	mysql -uroot < /usr/share/zoneminder/db/zm_create.sql
+	mysql -uroot -e "grant all on zm.* to 'zmuser'@localhost identified by 'zmpass';"
+	mysqladmin -uroot reload
+	mysql -sfu root < "mysql_secure_installation.sql"
+	rm mysql_secure_installation.sql
+	mysql -sfu root < "mysql_defaults.sql"
+	rm mysql_defaults.sql
 else
+        sed  -i "s|ZM_DB_HOST=localhost|ZM_DB_HOST=$ZM_DB_HOST|" /etc/zm/zm.conf
+        sed  -i "s|MYSQL_ROOT_PASSWORD=mysqlpsswd|MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD|" /etc/zm/zm.conf
+	sed  -i "s|ZM_DB_USER=zmuser|ZM_DB_USER=$ZM_DB_USER|" /etc/zm/zm.conf
+	sed  -i "s|ZM_DB_USER=zmpass|ZM_DB_USER=$ZM_DB_PASS|" /etc/zm/zm.conf
 fi
 
 
-        #if ENV variables are provided in container use it as is, if not left use defaults
-        ZM_DB_HOST=${ZM_DB_HOST:-localhost} && \
-	ZM_DB_PORT=${ZM_DB_PORT:-3306} && \
-	MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-mysqlpsswd} && \
-	
-	mysql -uroot < /usr/share/zoneminder/db/zm_create.sql && \
-	mysql -uroot -e "grant all on zm.* to 'zmuser'@localhost identified by 'zmpass';" && \
-	mysqladmin -uroot reload && \
-	mysql -sfu root < "mysql_secure_installation.sql" && \
-	rm mysql_secure_installation.sql && \
-	mysql -sfu root < "mysql_defaults.sql" && \
-	rm mysql_defaults.sql && \
-	
-        sed  -i "s|ZM_DB_HOST=localhost|ZM_DB_HOST=$ZM_DB_HOST|" /etc/zm/zm.conf && \
-        sed  -i "s|MYSQL_ROOT_PASSWORD=mysqlpsswd|MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD|" /etc/zm/zm.conf && \
+
 	
 	mysql -uroot < /usr/share/zoneminder/db/zm_create.sql && \
 	mysql -uroot -e "grant all on zm.* to 'zmuser'@localhost identified by 'zmpass';" && \
