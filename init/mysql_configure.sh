ba0819ee8db694ec3e4bd79fc2c2453d44ad5127
SmartHomeBeginner <htpcbeginner@gmail.com>
2019-03-28 09:00:01 -0400

    Update mysql_configure.sh

--- init/mysql_configure.sh
+++ init/mysql_configure.sh
@@ -9,7 +9,7 @@ MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-mysqlpsswd}
 ZM_DB_USER=${ZM_DB_USERNAME:-zmuser}
 ZM_DB_PASS=${ZM_DB_PASSWORD:-zmpass}
 	
-if [[ $ZM_DB_HOST == "localhost" && $MYSQL_ROOT_PASSWORD != "mysqlpsswd" ]]; then
+if [[ $ZM_DB_HOST == "localhost" ]]; then
 	mysql -uroot < /usr/share/zoneminder/db/zm_create.sql
 	mysql -uroot -e "grant all on zm.* to 'zmuser'@localhost identified by 'zmpass';"
 	mysqladmin -uroot reload
@@ -18,19 +18,10 @@ if [[ $ZM_DB_HOST == "localhost" && $MYSQL_ROOT_PASSWORD != "mysqlpsswd" ]]; the
 	mysql -sfu root < "mysql_defaults.sql"
 	rm mysql_defaults.sql
 else
-        sed  -i "s|ZM_DB_HOST=localhost|ZM_DB_HOST=$ZM_DB_HOST|" /etc/zm/zm.conf
-        sed  -i "s|MYSQL_ROOT_PASSWORD=mysqlpsswd|MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD|" /etc/zm/zm.conf
+        sed  -i "s|ZM_DB_HOST=localhost|ZM_DB_HOST=$ZM_DB_HOST:$ZM_DB_PORT|" /etc/zm/zm.conf
 	sed  -i "s|ZM_DB_USER=zmuser|ZM_DB_USER=$ZM_DB_USER|" /etc/zm/zm.conf
 	sed  -i "s|ZM_DB_USER=zmpass|ZM_DB_USER=$ZM_DB_PASS|" /etc/zm/zm.conf
+	mysql -uroot -p "$MYSQL_ROOT_PASSWORD" < /usr/share/zoneminder/db/zm_create.sql
+	mysql -uroot -e "grant all on zm.* to '$ZM_DB_USER'@localhost identified by 'ZM_DB_PASS';"
+	mysqladmin -uroot -p "$MYSQL_ROOT_PASSWORD" reload
 fi
-
-
-
-	
-	mysql -uroot < /usr/share/zoneminder/db/zm_create.sql && \
-	mysql -uroot -e "grant all on zm.* to 'zmuser'@localhost identified by 'zmpass';" && \
-	mysqladmin -uroot reload && \
-	mysql -sfu root < "mysql_secure_installation.sql" && \
-	rm mysql_secure_installation.sql && \
-	mysql -sfu root < "mysql_defaults.sql" && \
-	rm mysql_defaults.sql
